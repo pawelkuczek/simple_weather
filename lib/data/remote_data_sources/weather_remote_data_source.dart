777e17d5f6185514f30f095a8ddd60757d0b0441
Paweł Kuczek <pawel.kuczek@vp.pl>
2023-06-02 17:16:02 +0200

    Add .fromJson method to our model

--- lib/data/remote_data_sources/weather_remote_data_source.dart
+++ lib/data/remote_data_sources/weather_remote_data_source.dart
@@ -1,13 +1,11 @@
 import 'package:dio/dio.dart';
 
-
 class WeatherRemoteDataSource {
-  Future<Map<String,dynamic>?> getWeatherData({
+  Future<Map<String, dynamic>?> getWeatherData({
     required String city,
   }) async {
     final response = await Dio().get<Map<String, dynamic>>(
         'http://api.weatherapi.com/v1/current.json?key=610d99df2ee34c55966200419233105&q=$city&aqi=no');
-
-
+    return response.data;
   }
 }
